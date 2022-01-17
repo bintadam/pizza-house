6e0a0e715bd178ed08d823b610f4d68cd87ab1e2
zakiya adam <zakiyaadan12@gmail.com>
2022-01-17 18:03:46 +0300

    click function

--- js/script.js
+++ js/script.js
@@ -7,3 +7,14 @@ function Getpizza( name,size,crust,topping, total ){
   this.topping = topping;
   this.total = total;
 }
+$(document).ready(function(){
+  $(button.proceed).click(function(event){
+    let pizzaName = $(".name option:selected").val();
+    let pizzaSize = $("#size option:selected").val();
+    let pizzaCrust = $("#crust option:selected").val();
+    let pizzaToppings =[]
+    $.each($("input[name='toppings']:checked"), function(){            
+      pizzaTopping.push($(this).val());
+  });
+  console.log(pizzaTopping.join(", "));
+}
