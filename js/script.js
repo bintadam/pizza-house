9528003da0766741f99349cde9f05f06dd8a0adf
zakiya adam <zakiyaadan12@gmail.com>
2022-01-17 18:37:44 +0300

    added if and if else for finalmessage

--- js/script.js
+++ js/script.js
@@ -152,4 +152,17 @@ $(document).ready(function(){
       let phone = $("input#phone").val();
       let location = $("input#location").val();
 
+      if ($("input#name").val() && $("input#phone").val() && $("input#location").val()!=""){
+  
+        $("#finallmessage").append(person+", We have recieved your order and it will be delivered to you at "+location+ ". Prepare sh. "+deliceryamount);
+        $("#totalbill").hide();
+        $("#finallmessage").slideDown(1200);
+      }
+      else {
+        alert("Please fill in the details for delivery!");
+        $(".delivery").show();
+        $("button#final-order").show();
+      }
+    });
+
 }
